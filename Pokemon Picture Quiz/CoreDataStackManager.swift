c8723515315ac569a01b5602903aea7c9e556d73
Brian Ortega <brian.ortega@gmail.com>
2015-06-16 03:53:41 -0400

    First commit. Sets up tab view controller and core data stuff.

---
+++ Pokemon Picture Quiz/CoreDataStackManager.swift
@@ -0,0 +1,74 @@
+//
+//  CoreDataStackManager.swift
+//  Pokemon Picture Quiz
+//
+//  Created by Brian Ortega on 6/16/15.
+//  Copyright (c) 2015 Brian Ortega. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStackManager {
+    
+    // MARK: - Core Data stack
+    
+    class func sharedInstance() -> CoreDataStackManager {
+        struct Static {
+            static let instance = CoreDataStackManager()
+        }
+    
+        return Static.instance
+    }
+    
+    lazy var applicationDocumentsDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls[urls.count-1] as! NSURL
+    }()
+
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        let modelURL = NSBundle.mainBundle().URLForResource("Pokemon_Picture_Quiz", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("Pokemon_Picture_Quiz.sqlite")
+        var error: NSError? = nil
+        var failureReason = "There was an error creating or loading the application's saved data."
+        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
+            coordinator = nil
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            dict[NSUnderlyingErrorKey] = error
+            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            NSLog("Unresolved error \(error), \(error!.userInfo)")
+            abort()
+        }
+        
+        return coordinator
+    }()
+
+    lazy var managedObjectContext: NSManagedObjectContext? = {
+        let coordinator = self.persistentStoreCoordinator
+        if coordinator == nil {
+            return nil
+        }
+        var managedObjectContext = NSManagedObjectContext()
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        if let moc = self.managedObjectContext {
+            var error: NSError? = nil
+            if moc.hasChanges && !moc.save(&error) {
+                NSLog("Unresolved error \(error), \(error!.userInfo)")
+                abort()
+            }
+        }
+    }
+}
---
+++ Pokemon Picture Quiz/PokeQuizViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PokeQuizViewController.swift
+//  Pokemon Picture Quiz
+//
+//  Created by Brian Ortega on 6/16/15.
+//  Copyright (c) 2015 Brian Ortega. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class PokeQuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
